a11afa18e20b0680b8665b5af96023714f439a1a
DawnSee0823 <nazarnesterov21428@gmail.com>
2023-03-06 23:14:54 -0500

    feat: update admin panel

---
+++ router/admin.js
@@ -0,0 +1,10 @@
+const router = require('express').Router();
+const Admin = require('../controller/Admin');
+
+// router.get('/all', Admin.getItems);
+// router.post('/', Category.setItems);
+// router.post('/addItem', Category.addItem);
+// router.get('/myItem', Category.getMyItems);
+// router.post('/delete', Category.deleteItem);
+
+module.exports = router;
